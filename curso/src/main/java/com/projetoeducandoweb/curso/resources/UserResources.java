b908f114d06fb2fb17b835af897335cdf57d44db
Alberti Luigy <albertiluigy@gmai.com>
2026-09-03 21:28:35 -0300

    Camada de serviço, registro de componentes

--- curso/src/main/java/com/projetoeducandoweb/curso/resources/UserResources.java
+++ curso/src/main/java/com/projetoeducandoweb/curso/resources/UserResources.java
@@ -5,22 +5,29 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetoeducandoweb.curso.entities.User;
-import com.projetoeducandoweb.curso.repositories.UserRepository;
+import com.projetoeducandoweb.curso.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
     
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService; // Injetando o objeto UserService na variável userService
     
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
-        List<User> list = userRepository.findAll();
-        return ResponseEntity.ok().body(list);
+    public ResponseEntity<List<User>> findAll(){ // Método para buscar todos os usuários
+        List<User> list = userService.findAll(); // Chamando o método findAll do serviço
+        return ResponseEntity.ok().body(list); // Retornando todos os usuários
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){ // Método para buscar um usuário pelo ID
+        User obj = userService.findById(id); // Chamando o método findById do serviço com o ID do usuário
+        return ResponseEntity.ok().body(obj); // Retornando o usuário
     }
 }
---
+++ curso/src/main/java/com/projetoeducandoweb/curso/service/UserService.java
@@ -0,0 +1,31 @@
+package com.projetoeducandoweb.curso.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetoeducandoweb.curso.entities.User;
+import com.projetoeducandoweb.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired // Injeção de dependência
+    // Injetando o objeto UserRepository na variável repository
+    private UserRepository repository;
+    
+    public List<User> findAll(){ // Método para buscar todos os usuários
+        return repository.findAll(); // Retornando todos os usuários
+    }
+
+    public User findById(Long id){ // Método para buscar um usuário pelo ID
+        Optional<User> obj = repository.findById(id); // Retornando o usuário pelo ID
+        return obj.get(); // Retornando o usuário
+    }
+
+    
+
+    
+}
